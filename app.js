13d10af0c0f094d86661ce5e93edd1c347518341
MASTER0811 <85328244+MASTER0811@users.noreply.github.com>
2022-05-22 21:47:52 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+let input = document.querySelector("input")
+let htext = document.querySelector(".localtime")
+let container = document.querySelector(".hide-section")
+let btn = document.querySelector("button")
+let date = new Date()
+let hours = date.getHours()
+
+
+htext.innerHTML = hours
+
+
+btn.onclick = () =>{
+    if(input.value == hours){
+        console.log(`${hours} true`);
+        container.classList.add("hide")
+    }else{
+        container.classList.remove("hide")
+    }
+    let ivalue = input.value
+    if(ivalue == 10 && ivalue == 13 && ivalue == 13 && ivalue == 13 && ivalue == 13)
+    if(input.value > 24){
+        console.log("err");
+    }else{
+        console.log("value true");
+    }
+}
